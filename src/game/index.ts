cb06ef754f46b932a4c420cb749c5ae352b2db8c
Everath <eve@everath.net>
2021-09-15 21:47:04 +0200

    Make GameService be able to read messages.

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,3 +1,20 @@
+import { Message } from 'discord.js'
+
 export class GameService {
-    
+
+    constructor(){
+        this.onMessage = this.onMessage.bind(this);
+    }
+
+    async onMessage(message: Message) {
+        if(message.author.bot) return;
+
+        let currentMessage = message.content;
+        await console.log(currentMessage);
+
+        
+
+        
+    }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import { UserService } from './user';
 import { GameService } from './game';
 import config from './config.json';
 
-const client = new Client<true>({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client<true>({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const userService = new UserService(client);
-//const gameService = new GameService();
+const gameService = new GameService();
 
 
 client.on("ready", userService.onReady);
 client.on("guildCreate", userService.onGuildCreate);
 client.on("guildDelete", userService.onGuildDelete);
-//client.on("message", gameService.onMessage);
+client.on("message", gameService.onMessage);
+/*client.on("message", async message => {
+    if(message.author.bot) return;
+    await message.channel.send("B-Baka!");
+});*/
 client.login(config.token);
